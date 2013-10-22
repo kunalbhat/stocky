f57d7068eb3fdc576299bddd7740d3b8fce67b91
Kunal Bhat <kunalbhat@gmail.com>
2013-10-22 09:53:28 -0500

    Add market status banner

--- decorators/stocklist_decorator.rb
+++ decorators/stocklist_decorator.rb
@@ -5,7 +5,7 @@ class StockListDecorator
     close = Time.utc(now.year, now.day, now.month, 16)
     open = Time.utc(now.year, now.day, now.month, 8)
 
-    if now > close && now > open
+    if now > close && now < open
       "<div class='market-status'>Market Closed</div>"
     else
       "<div class='market-status'>Market Open &ndash; #{Time.now}</div>"
